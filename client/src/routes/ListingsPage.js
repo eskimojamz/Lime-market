6344cb71d37e4de38862580c76e86da3e398f109
James Kim <juleskondo@gmail.com>
2021-11-03 15:17:27 +0900

    pagination

--- client/src/routes/ListingsPage.js
+++ client/src/routes/ListingsPage.js
@@ -1,4 +1,5 @@
-import { useEffect } from 'react'
+import { useState, useEffect, useMemo } from 'react'
+import Pagination from '../pagination/Pagination'
 import Listing from '../components/Listing'
 import Loading from '../components/Loading'
 
@@ -6,30 +7,53 @@ import { useDispatch, useSelector } from 'react-redux'
 
 import { motion} from 'framer-motion'
 import { getListings } from '../actions/actions'
-
+import { fetchListings } from '../api/api'
 
 const ListingsPage = () => {
   const dispatch = useDispatch()
-  const listings = useSelector((state) => state.listings)
-  console.log(listings)
+  const [loading, setLoading] = useState(true)
+  const [listings, setListings] = useState()
+  const [currentPage, setCurrentPage] = useState(1)
+  const PageSize = 1
 
   useEffect(() => {
-    dispatch(getListings())
+    fetchListings().then(response => setListings(response.data))
   }, [])
 
+  const currentData = useMemo(() => {
+    const firstPageIndex = (currentPage - 1) * PageSize;
+    const lastPageIndex = firstPageIndex + PageSize;
+    return listings?.slice(firstPageIndex, lastPageIndex);
+  }, [listings, currentPage]);
+
+  console.log(listings)
+  console.log(currentData)
+
+  
+
   return (
-      listings.length > 0 ?
+      currentData?.length > 0 ?
         (
+        <>
         <motion.div 
           className="listings-grid"
           initial={{ y: 20, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
           transition={{ delay: 0.5, duration: 0.25 }}
         >
-        {listings.map(listing => 
+        {currentData.map(listing => 
           <Listing listing={listing} />
         )}
         </motion.div>
+
+        <Pagination
+          className="pagination-bar"
+          currentPage={currentPage}
+          totalCount={listings?.length}
+          pageSize={PageSize}
+          onPageChange={page => setCurrentPage(page)}
+        />
+        </>
         ) :
       <Loading />   
   )
