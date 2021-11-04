f3db9a45e7620c54b05c0f539e8457f11a8f7907
James Kim <juleskondo@gmail.com>
2021-11-04 18:51:57 +0900

    desktop listing menu css

--- client/src/components/EditMenuBackdrop.js
+++ client/src/components/EditMenuBackdrop.js
@@ -10,6 +10,9 @@ export default function EditMenuBackdrop({ toggleMenu, setToggleMenu, handleEdit
                 <div className={`edit-menu-mobile ${toggleMenu && "edit-menu-mobile-open"}`}>
                     <button className="button-menu-edit" onClick={handleEdit}><h5 >Edit</h5></button>
                     <button className="button-menu-delete" onClick={handleDelete}><h5>Delete</h5></button>
+                    <div className="button-menu-close-mobile-container">
+                        <button className="button-menu-close-mobile"><h5>Close</h5></button>
+                    </div>
                 </div>
             </div>
         
