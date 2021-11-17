b0a9b126ada5e1126c3272d82cf80400f608d30b
James Kim <juleskondo@gmail.com>
2021-11-17 17:05:16 +0900

    hero listings

--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import {useEffect} from 'react'
 import { useHistory } from 'react-router-dom'
 import herolisting from '../assets/herolisting.svg'
 import herobg from '../assets/herobg.svg'
 import { motion } from 'framer-motion'
+import { useDispatch, useSelector } from 'react-redux'
+import { getListings } from '../actions/actions'
+import Listing from '../components/Listing'
 
 const Home = () => {
     const history = useHistory()
+    const dispatch = useDispatch()
+    const listings = useSelector(state => state.listings)
+    
+    useEffect(() => {
+        dispatch(getListings())
+    }, [])
 
     return (
         <>
@@ -72,6 +81,28 @@ const Home = () => {
             animate={{ y: 0, opacity: 1 }}
         >
             {/* Popular Listings */}
+            <motion.div className="hero-bottom-h"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 0.35, duration: 0.25 }}
+            >
+                <h3>Recent Listings</h3>
+            </motion.div>
+            <div className="listings-grid">
+                {listings.slice(-3).map(listing => 
+                    <Listing listing={listing} />
+                )}
+            </div>
+            <motion.button 
+                className="button-primary" 
+                initial={{ y: 20, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ delay: 0.5, duration: 0.25 }}
+                onClick={() => {
+                    history.push('/listings')
+                }}>
+                    See More
+            </motion.button>
         </motion.div>
         </>
     )
